52e41836ce4f3c51f541a4a088376e44e97ce53a
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-18 02:47:18 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright} style={{textAlign: 'center'}}>
+      <div className={styles.copyright} style="text-align: center;">
         Copyright 2023 {config.author}
       </div>
 
