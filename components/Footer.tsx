bd870e40a53f0d21c496797b6644380a3d1785c7
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-18 01:35:18 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-
+/*
 import { FaEnvelopeOpenText } from '@react-icons/all-files/fa/FaEnvelopeOpenText'
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
 import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
@@ -9,22 +9,24 @@ import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube'
 import { FaZhihu } from '@react-icons/all-files/fa/FaZhihu'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+*/
 
 import * as config from '@/lib/config'
-import { useDarkMode } from '@/lib/use-dark-mode'
+//import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
 
 // TODO: merge the data and icons from PageSocial with the social links in Footer
 
 export const FooterImpl: React.FC = () => {
+  /* remove dark
   const [hasMounted, setHasMounted] = React.useState(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
 
   const onToggleDarkMode = React.useCallback(
     (e) => {
       e.preventDefault()
-      toggleDarkMode()
+      toggleMode()
     },
     [toggleDarkMode]
   )
@@ -32,11 +34,11 @@ export const FooterImpl: React.FC = () => {
   React.useEffect(() => {
     setHasMounted(true)
   }, [])
-
+  */
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright 2023 {config.author}</div>
-      
+      /*
       <div className={styles.settings}>
         {hasMounted && (
           <a
@@ -50,7 +52,7 @@ export const FooterImpl: React.FC = () => {
           </a>
         )}
       </div>
-      
+      */
       <div className={styles.social}>
         {config.twitter && (
           <a
