063ac84eb152080d3efb07fa2f8af59eaef07433
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-18 01:43:39 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,14 +12,14 @@ import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 
 
 import * as config from '@/lib/config'
-//import { useDarkMode } from '@/lib/use-dark-mode'
+import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
 
 // TODO: merge the data and icons from PageSocial with the social links in Footer
 
 export const FooterImpl: React.FC = () => {
-  /* remove dark
+
   const [hasMounted, setHasMounted] = React.useState(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
 
@@ -34,25 +34,25 @@ export const FooterImpl: React.FC = () => {
   React.useEffect(() => {
     setHasMounted(true)
   }, [])
-  */
+
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright 2023 {config.author}</div>
-      /*
+
       <div className={styles.settings}>
         {hasMounted && (
           <a
-            className={styles.toggleDarkMode}
+            className={/*styles.toggleDarkMode*/}
             href='#'
             role='button'
-            onClick={onToggleDarkMode}
+            onClick={/*onToggleDarkMode*/}
             title='Toggle dark mode'
           >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+            {/*isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />*/}
           </a>
         )}
       </div>
-      */
+
       <div className={styles.social}>
         {config.twitter && (
           <a
