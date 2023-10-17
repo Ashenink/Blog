a326367cb05cfaf5d7dfacd05321886647d51bbf
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-18 02:09:54 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -50,6 +50,22 @@ export const FooterImpl: React.FC = () => {
           >
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
+             <svg
+            width='0' //80 original
+            height='0' //80 original
+            viewBox='0 0 250 250'
+            style={{
+              fill: '#70B7FD',
+              color: '#fff',
+              position: 'absolute',
+              zIndex: 1001,
+              top: 0,
+              right: 0,
+              border: 0,
+              transform: 'scale(1, 1)'
+            }}
+            aria-hidden='true'
+          >
         )}
       </div>
 
