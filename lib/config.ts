ae18e700862002ae1e012dc028f1616b6b22838b
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-18 13:37:23 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -110,7 +110,10 @@ export const navigationLinks: Array<NavigationLink | null> = getSiteConfig(
 )
 
 // Optional site search
-export const isSearchEnabled: boolean = getSiteConfig('isSearchEnabled', true)
+export const isSearchEnabled: boolean = getSiteConfig(
+  'isSearchEnabled',
+  false
+)
 
 // ----------------------------------------------------------------------------
 
