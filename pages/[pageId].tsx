a03a0f9a75fa684ab1c046ac784e8505e5bee9e0
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-20 01:39:47 +0800

    Update [pageId].tsx

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -53,4 +53,23 @@ export async function getStaticPaths() {
 export default function NotionDomainDynamicPage(props) {
   return <NotionPage {...props} />
 
+
+
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="Ashenink/Blog"
+        data-repo-id="R_kgDOKhfGnA"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOKhfGnM4CaRlu"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async
+
+
 }
