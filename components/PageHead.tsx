ce9abf4b98e64fb17484d6b7addf25921e7634a9
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-20 02:18:43 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -82,26 +82,25 @@ export const PageHead: React.FC<
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
+
+      <script src="https://giscus.app/client.js"
+      data-repo="Ashenink/Blog"
+      data-repo-id="R_kgDOKhfGnA"
+      data-category="Announcements"
+      data-category-id="DIC_kwDOKhfGnM4CaRlu"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="bottom"
+      data-theme="preferred_color_scheme"
+      data-lang="en"
+      crossorigin="anonymous"
+      async>
+    </script>
+      
     </Head>
     
-
-    
-    <script src="https://giscus.app/client.js"
-        data-repo="Ashenink/Blog"
-        data-repo-id="R_kgDOKhfGnA"
-        data-category="Announcements"
-        data-category-id="DIC_kwDOKhfGnM4CaRlu"
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="bottom"
-        data-theme="preferred_color_scheme"
-        data-lang="en"
-        crossorigin="anonymous"
-        async>
-      </script>
-   
 
   )
 }
