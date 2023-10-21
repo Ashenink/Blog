dc261d8cc743b67aee1a544fd5b12ee9c83b6b79
Ashenink <148219063+Ashenink@users.noreply.github.com>
2023-10-21 15:14:07 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,24 +39,26 @@ export const FooterImpl: React.FC = () => {
     <footer className={styles.footer}>
       <div className={styles.copyright} style={{textAlign: 'center'}}>
         Copyright 2023 {config.author}
-        
-        <script src="https://giscus.app/client.js"
-        data-repo="Ashenink/Blog"
-        data-repo-id="R_kgDOKhfGnA"
-        data-category="Announcements"
-        data-category-id="DIC_kwDOKhfGnM4CaRlu"
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="bottom"
-        data-theme="dark_tritanopia"
-        data-lang="en"
-        crossorigin="anonymous"
-        async>
-</script>
       </div>
-
+      
+      <div style={{textAlign: 'center'}}>
+              <script src="https://giscus.app/client.js"
+              data-repo="Ashenink/Blog"
+              data-repo-id="R_kgDOKhfGnA"
+              data-category="Announcements"
+              data-category-id="DIC_kwDOKhfGnM4CaRlu"
+              data-mapping="pathname"
+              data-strict="0"
+              data-reactions-enabled="1"
+              data-emit-metadata="0"
+              data-input-position="bottom"
+              data-theme="dark_tritanopia"
+              data-lang="en"
+              crossorigin="anonymous"
+              async>
+      </script>
+      </div>
+      
       <div className={styles.settings} style={{fontSize: 0}}>
         {hasMounted && (
           <a
