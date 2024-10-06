ab03f1a02062aacc69f349b8b02d2a2b87cd0305
Ao Shen <aoshen.dev@gmail.com>
2024-10-06 13:57:11 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,7 @@ export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright} style={{textAlign: 'center'}}>
-        Copyright 2023 - Present {config.author}
+        Copyright © 2023 - Present @ {config.author}
       </div>
       
       <div className={styles.settings} style={{fontSize: 0}}>
