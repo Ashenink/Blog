6d0670c3540dd1ffdde0303e1f882cab9117a710
Ao Shen <aoshen.dev@gmail.com>
2024-10-11 14:21:53 +0800

    icon update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,16 +11,16 @@ export default class MyDocument extends Document {
         <Html lang='en'>
           <Head>
             
-            <link rel='shortcut icon' href= 'https://img.notionusercontent.com/s3/prod-files-secure%2Fc9d47a73-9b73-4e98-9885-3c983263bd1e%2F91b583d7-eb0a-4295-a830-f1d7c4038465%2Fandroid-chrome-512x512.png/size/w=1420?exp=1728713641&sig=Xq6RT60eOHp0fK9zj045o9aVv_sBLFf1mcOJK2LsT3Q' />
+            <link rel='shortcut icon' href= 'https://img.notionusercontent.com/s3/prod-files-secure%2Fc9d47a73-9b73-4e98-9885-3c983263bd1e%2F39be6149-9c41-4cc1-9297-a5696a5469e0%2Fimage.png/size/w=2000?exp=1728714026&sig=CrRyPAxeL5bXblSGonGY8sWTOzv8CvJYxf7VaCbfLlo' />
 
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='https://img.notionusercontent.com/s3/prod-files-secure%2Fc9d47a73-9b73-4e98-9885-3c983263bd1e%2F18bdb18a-a7f3-4da0-8655-4e0fa8d290fc%2Ffavicon-32x32.png/size/w=2000?exp=1728713599&sig=Rrq3qIBA-S0DzZkZ0M7_zqs-1ikAykLjRBGUm2tdKpo'
+              href='https://img.notionusercontent.com/s3/prod-files-secure%2Fc9d47a73-9b73-4e98-9885-3c983263bd1e%2F39be6149-9c41-4cc1-9297-a5696a5469e0%2Fimage.png/size/w=2000?exp=1728714026&sig=CrRyPAxeL5bXblSGonGY8sWTOzv8CvJYxf7VaCbfLlo'
             />
 
-            <link rel='manifest' href='/icon/site.webmanifest' />
+            <link rel='manifest' href='/manifest.json' />
 
           </Head>
           
