a9ea282be3805131811fb8c9727cbc143878f031
Ao Shen <aoshen.dev@gmail.com>
2024-10-11 13:39:09 +0800

    update icon
    modified PageHead.tsx at the bottom // add a folder icon

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -82,6 +82,12 @@ export const PageHead: React.FC<
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png"/>
+      <link rel="manifest" href="/icon/site.webmanifest"/>
+
     </Head>
   )
 }
